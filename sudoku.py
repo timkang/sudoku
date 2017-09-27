0941b5691efac6fa0f0069149162c5e4f0670cdb
Tim <got.ownage@gmail.com>
2017-09-27 15:28:08 -0700

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -38,14 +38,16 @@ def goat(x, y, num):
   for count1 in range(minX, maxX):
     for count2 in range(minY, maxY):
       dog[count1][count2] = dog[count1][count2] - 100 - num
-      
+      if dog[count1][count2] < 200:
+        cat.append([count1, count2, dog[count1][count2]])
   for count1 in range(0,9):
     for count2 in range(0,9):
       if count1 == x and (count2 < minY or count2 >= maxY):
         dog[count1][count2] = dog[count1][count2] - 100 - num
       if count2 == y and (count1 < minX or count1 >= maxX):
         dog[count1][count2] = dog[count1][count2] - 100 - num
-  
+      if dog[count1][count2] < 200:
+        cat.append([count1, count2, dog[count1][count2]])
   dog[x][y] = 100 + num;
   for line in dog:
     print(line)
@@ -53,4 +55,8 @@ def goat(x, y, num):
 while True:
   person = input("Please input x, y, num : ").split(",")
   goat(int(person[0]), int(person[1]), int(person[2]))
+  while len(cat) > 0:
+    c = cat.pop(0)
+    goat(c[0],c[1],c[2])
+    
   
