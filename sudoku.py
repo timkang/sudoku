158ce0a0150a04f71e1e23f47af6ede979878108
Tim <got.ownage@gmail.com>
2017-09-28 17:42:03 -0700

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -11,44 +11,48 @@ dog = [
   ]
 
 cat = []
+owl = []
 
 #0-3 3-6 6-9
 def goat(x, y, num):
-
-  if x < 3:
-    minX = 0
-    maxX = 3
-  elif x < 6:
-    minX = 3
-    maxX = 6
-  elif x < 9:
-    minX = 6
-    maxX = 9
-
-  if y < 3:
-    minY = 0
-    maxY = 3
-  elif y < 6:
-    minY = 3
-    maxY = 6
-  elif y < 9:
-    minY = 6
-    maxY = 9    
-
-  for count1 in range(minX, maxX):
-    for count2 in range(minY, maxY):
-      dog[count1][count2] = dog[count1][count2] - 100 - num
-      if dog[count1][count2] < 200:
-        cat.append([count1, count2, dog[count1][count2]])
-  for count1 in range(0,9):
-    for count2 in range(0,9):
-      if count1 == x and (count2 < minY or count2 >= maxY):
-        dog[count1][count2] = dog[count1][count2] - 100 - num
-      if count2 == y and (count1 < minX or count1 >= maxX):
-        dog[count1][count2] = dog[count1][count2] - 100 - num
-      if dog[count1][count2] < 200:
-        cat.append([count1, count2, dog[count1][count2]])
-  dog[x][y] = 100 + num;
+  if not [x,y] in owl:
+    if x < 3:
+      minX = 0
+      maxX = 3
+    elif x < 6:
+      minX = 3
+      maxX = 6
+    elif x < 9:
+      minX = 6
+      maxX = 9
+  
+    if y < 3:
+      minY = 0
+      maxY = 3
+    elif y < 6:
+      minY = 3
+      maxY = 6
+    elif y < 9:
+      minY = 6
+      maxY = 9    
+  
+    for count1 in range(minX, maxX):
+      for count2 in range(minY, maxY):
+        if not dog[count1][count2] < 200:
+          dog[count1][count2] = dog[count1][count2] - 100 - num
+          if dog[count1][count2] < 200:
+            cat.append([count1, count2, dog[count1][count2]])
+    for count1 in range(0,9):
+      for count2 in range(0,9):
+        if not dog[count1][count2] < 200:
+          if count1 == x and (count2 < minY or count2 >= maxY):
+            dog[count1][count2] = dog[count1][count2] - 100 - num
+          if count2 == y and (count1 < minX or count1 >= maxX):
+            dog[count1][count2] = dog[count1][count2] - 100 - num
+          if dog[count1][count2] < 200:
+            cat.append([count1, count2, dog[count1][count2]])
+    dog[x][y] = 100 + num;
+    owl.append([x,y])
   for line in dog:
     print(line)
 
